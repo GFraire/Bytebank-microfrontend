f6486b754eb348b6e8e778820a3a15eb66b72712
Henrique Moura <henrique.bmoura@yahoo.com.br>
2025-07-12 02:13:27 -0300

    feat: add webpack configuration for main app with CSS support

--- main-app/webpack.config.js
+++ main-app/webpack.config.js
@@ -4,55 +4,47 @@ const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPl
 module.exports = {
   mode: "development",
   output: {
-    publicPath: 'http://localhost:3001/',
-    filename: '[name].js',
+    publicPath: "http://localhost:3000/",
+    filename: "[name].js",
   },
   devServer: {
-    port: 3001,
-    historyApiFallback: { index: 'index.html' },
+    port: 3000,
+    historyApiFallback: { index: "index.html" },
     hot: true,
     headers: {
-      'Access-Control-Allow-Origin': '*',
+      "Access-Control-Allow-Origin": "*",
     },
   },
   resolve: {
-    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
   },
   module: {
     rules: [
       {
         test: /\.(js|jsx|ts|tsx)$/,
-        loader: 'babel-loader',
+        loader: "babel-loader",
         exclude: /node_modules/,
         options: {
-          presets: [
-            '@babel/preset-react',
-            '@babel/preset-typescript'
-          ],
+          presets: ["@babel/preset-react", "@babel/preset-typescript"],
         },
       },
       {
         test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'postcss-loader',
-        ],
+        use: ["style-loader", "css-loader", "postcss-loader"],
       },
     ],
   },
   plugins: [
     new ModuleFederationPlugin({
-      name: "home",
-      filename: "remoteEntry.js",
-      exposes: {
-        './Home': './src/bootstrap',
+      name: "main_app",
+      remotes: {
+        home: "home@http://localhost:3001/remoteEntry.js",
       },
       shared: {
         react: {
           singleton: true,
         },
-        'react-dom': {
+        "react-dom": {
           singleton: true,
         },
       },
