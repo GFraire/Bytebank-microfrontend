dd1ae3397589fda148e25d75a18d39be0d5e0ebd
Henrique Moura <henrique.bmoura@yahoo.com.br>
2025-07-12 02:16:33 -0300

    feat: replace _app.js with _app.tsx and update Home component heading

---
+++ main-app/src/pages/_app.tsx
@@ -0,0 +1,8 @@
+import "../../../design-system/globals.css";
+import { AppProps } from "next/app";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
