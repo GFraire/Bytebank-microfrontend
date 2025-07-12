01c0e8212b998333887404af48d094d73cf7ae2f
Henrique Moura <henrique.bmoura@yahoo.com.br>
2025-07-12 13:18:13 -0300

    feat: add Tailwind CSS configuration and global styles for add-transaction, dashboard, sidebar, and transactions

--- design-system/tailwind.config.js
+++ design-system/tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./globals.css'],
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}', // Ajuste conforme necessário
+    './globals.css',
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
 
+// design-system/globals.css
---
+++ transactions/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./globals.css'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
