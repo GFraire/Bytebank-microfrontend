e12c9682192a11f4822aac23d06ccd4910951592
Gabriel <gabriefreiresa@gmail.com>
2025-07-12 22:05:03 -0300

    feat: Configured tailwindcss for MFE sidebar

--- sidebar/src/App.tsx
+++ sidebar/src/App.tsx
@@ -1,11 +1,7 @@
 import React from "react";
 
 function AppSidebar() {
-  return (
-    <>
-      <h1>Sidebar</h1>
-    </>
-  );
+  return <h1 className="text-primary">Sidebar</h1>;
 }
 
 export default AppSidebar;
