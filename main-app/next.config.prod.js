6f75bfc33ebf7e35c98fe3389c2f3823fbd81b04
giovanna-godoy <giovannagodoy@icloud.com>
2025-07-21 21:54:32 -0300

    feat: Including vercel files

--- main-app/next.config.prod.js
+++ main-app/next.config.prod.js
@@ -6,15 +6,15 @@ const nextConfig = {
     const { isServer } = options;
     config.experiments = { topLevelAwait: true, layers: true };
     const BASE_URL = 'https://bytebank-'
-    const BASE_URL_END = '-grupo-9-fiapinhos-projects.vercel.app/remoteEntry.js'
+    const BASE_URL_END = '.vercel.app/remoteEntry.js'
     // URLs de produção dos micro frontends no Vercel
     const remotes = {
-      dashboard: `dashboard@${BASE_URL}dashboard-pprfovgb6${BASE_URL_END}`,
-      sidebar: `sidebar@${BASE_URL}sidebar-kwclcppi9${BASE_URL_END}`, 
-      transactions: `transactions@${BASE_URL}sidebar-kwclcppi9${BASE_URL_END}`,
-      addTransaction: `addTransaction@${BASE_URL}add-transaction-ot5r2p8qh${BASE_URL_END}`,
-      profile: `profile@${BASE_URL}profile-di1oy2tol${BASE_URL_END}`,
-      designSystem: `designSystem@${BASE_URL}design-system-grupo-9-fiapinhos-projects.vercel.app/remoteEntry.js`,
+      dashboard: `dashboard@${BASE_URL}dashboard${BASE_URL_END}`,
+      sidebar: `sidebar@${BASE_URL}sidebar${BASE_URL_END}`, 
+      transactions: `transactions@${BASE_URL}transactions${BASE_URL_END}`,
+      addTransaction: `addTransaction@${BASE_URL}add-transaction${BASE_URL_END}`,
+      profile: `profile@${BASE_URL}profile${BASE_URL_END}`,
+      designSystem: `designSystem@${BASE_URL}design-system${BASE_URL_END}`,
     };
 
     config.plugins.push(
