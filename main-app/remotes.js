d8d78bcd93eeb214ea208b5c80a68ef180cf2204
giovanna-godoy <giovannagodoy@icloud.com>
2025-07-24 20:44:56 -0300

    wip

--- main-app/remotes.js
+++ main-app/remotes.js
@@ -1,17 +1,12 @@
-const DESIGN_SYSTEM_DOMAIN = 'https://design-system-g9.vercel.app';
-const DASHBOARD_DOMAIN = 'https://bytebank-dashboard.vercel.app'; // Substitua pela URL real após o deploy
-const DEV_DOMAIN = 'http://localhost:4000';
-const DEV_DASHBOARD = 'http://localhost:3001';
-
 const isProd = process.env.NODE_ENV === 'production';
 
 const domains = {
-  designSystem: isProd ? DESIGN_SYSTEM_DOMAIN : DEV_DOMAIN,
-  dashboard: isProd ? DASHBOARD_DOMAIN : DEV_DASHBOARD,
-  sidebar: 'http://localhost:3002',
-  transactions: 'http://localhost:3003',
-  addTransaction: 'http://localhost:3004',
-  profile: 'http://localhost:3005',
+  designSystem: isProd ? 'https://design-system-g9.vercel.app' : 'http://localhost:4000',
+  dashboard: isProd ? 'https://dashboard-g9.vercel.app' : 'http://localhost:3001',
+  sidebar: isProd ? 'https://sidebar-g9.vercel.app' : 'http://localhost:3002',
+  transactions: isProd ? 'https://transactions-g9.vercel.app' : 'http://localhost:3003',
+  addTransaction: isProd ? 'https://add-transaction-g9.vercel.app' : 'http://localhost:3004',
+  profile: isProd ? 'https://profile-g9.vercel.app' : 'http://localhost:3005',
 };
 
 module.exports = {
