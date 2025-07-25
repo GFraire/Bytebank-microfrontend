a452aafe6d3268835e868bf2ae8d5726f6809052
giovanna-godoy <giovannagodoy@icloud.com>
2025-07-24 21:10:19 -0300

    wip

--- add-transaction/webpack.prod.js
+++ add-transaction/webpack.prod.js
@@ -7,7 +7,7 @@ module.exports = merge(common, {
   output: {
     filename: "[name].[contenthash].js",
     path: path.resolve(__dirname, "dist"),
-    publicPath: "/",
+    publicPath: "auto",
     clean: true,
   },
 });
