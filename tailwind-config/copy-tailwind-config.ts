0df11fcaeb792520e13aa1d446c3e1a9b27022eb
Gabriel <gabriefreiresa@gmail.com>
2025-08-02 13:50:11 -0300

    First commit

---
+++ tailwind-config/copy-tailwind-config.ts
@@ -0,0 +1,19 @@
+import fs from 'fs'
+import path from 'path'
+
+const sourcePath = path.resolve(__dirname, 'tailwind.config.js')
+
+const destinations = [
+  path.resolve(__dirname, '..', 'add-transaction', 'tailwind.config.js'),
+  path.resolve(__dirname, '..', 'profile', 'tailwind.config.js'),
+]
+
+destinations.forEach((dest) => {
+  fs.copyFile(sourcePath, dest, (err) => {
+    if (err) {
+      console.error(`❌ Erro ao copiar para ${dest}:`, err)
+    } else {
+      console.log(`✅ Copiado para ${dest}`)
+    }
+  })
+})
