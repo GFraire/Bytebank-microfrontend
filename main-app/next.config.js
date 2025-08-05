bdf5b9a075c07cfbb2e95315ebb9536b5e009aa1
Henrique Moura <henrique.bmoura@yahoo.com.br>
2025-08-05 13:34:42 -0300

    Add skipSharingNextInternals option to NextFederationPlugin configuration

--- main-app/next.config.js
+++ main-app/next.config.js
@@ -22,6 +22,7 @@ const nextConfig = {
         extraOptions: {
           exposePages: true,
           automaticAsyncBoundary: true,
+          skipSharingNextInternals: true,
         },
       })
     );
